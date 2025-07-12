0279762323574201682975d872c3355bb716001e
Evgeny Reznichenko <kusakyky@gmail.com>
2025-07-12 16:17:59 +0300

    check exist translate file

--- api/VOTApi.mjs
+++ api/VOTApi.mjs
@@ -1,12 +1,29 @@
 import { promisify } from 'node:util'
 import { exec } from 'node:child_process'
 import { makeDirectory } from 'make-dir'
+import path from 'node:path'
+import isFileExist from '../utils/isFileExist.mjs'
 
 const asyncExec = promisify(exec)
 
 export default class VOTApi {
+    static getOutputFileName(fileName, reslang) {
+        return `${fileName}--${reslang}`
+    }
+
+    static async isFileExist(outputDir, outputFileName, fileExt) {
+        const filePath = path.format({ dir: outputDir, name: outputFileName, ext: fileExt })
+        return await isFileExist(filePath)
+    }
+
     static async audio(publicUrl, outputDir, fileName, reslang = "ru") {
-        const { stderr } = await asyncExec(`./node_modules/.bin/vot-cli --reslang="${reslang}" --output="${outputDir}" --output-file="${fileName}--${reslang}" ${publicUrl}`)
+        const outputFileName = this.getOutputFileName(fileName, reslang)
+
+        if (await this.isFileExist(outputDir, outputFileName, '.mp3')) {
+            return
+        }
+
+        const { stderr } = await asyncExec(`./node_modules/.bin/vot-cli --reslang="${reslang}" --output="${outputDir}" --output-file="${outputFileName}" ${publicUrl}`)
 
         if (stderr) {
             throw stderr
@@ -14,7 +31,13 @@ export default class VOTApi {
     }
 
     static async sub(publicUrl, outputDir, fileName, reslang = "en") {
-        const { stderr } = await asyncExec(`./node_modules/.bin/vot-cli  --subs-srt --reslang="${reslang}" --output="${outputDir}" --output-file="${fileName}--${reslang}" ${publicUrl}`)
+        const outputFileName = this.getOutputFileName(fileName, reslang)
+
+        if (await this.isFileExist(outputDir, outputFileName, '.srt')) {
+            return
+        }
+
+        const { stderr } = await asyncExec(`./node_modules/.bin/vot-cli  --subs-srt --reslang="${reslang}" --output="${outputDir}" --output-file="${outputFileName}" ${publicUrl}`)
 
         if (stderr) {
             throw stderr
